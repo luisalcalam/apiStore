dba6d0e9ca8ebd7ae59269c71fce5e561a934a5f
Luis <luisalcala.ev@gmail.com>
2021-02-09 12:37:20 -0600

    Change conection

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     database: {
-        username: "root",
-        password: "root",
-        database: "store",
-        host: "localhost"
+        username: "udmf7uirv1x9na7c",
+        password: "oowFeeER2bBCBjIojcit",
+        database: "b1zbicbjixinymcnqo9p",
+        host: "b1zbicbjixinymcnqo9p-mysql.services.clever-cloud.com"
     }
 
 }
